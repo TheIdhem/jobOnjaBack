72f8d60043fe728d8675a692520b670df349dbce
mehdifallahi <mohammadf00900@gmail.com>
2019-03-01 01:02:39 +0330

    intializing at first server and try to implement poroject controller with servlet

---
+++ src/main/java/ir/joboona/Initializer.java
@@ -0,0 +1,22 @@
+package ir.joboona;
+
+import Solutions.Core.SolutionsApplication;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Initializer implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent event) {
+        // Do stuff during webapp's startup.
+        try {
+            String[] args = new String[100];
+            SolutionsApplication.getInstance().run(args);
+        } catch (Exception ex) {
+
+        }
+
+    }
+}
---
+++ src/main/java/ir/joboona/Presentation/Controllers/ProjectController_.java
@@ -0,0 +1,39 @@
+package ir.joboona.Presentation.Controllers;
+
+import Solutions.Core.SolutionsApplication;
+import ir.joboona.Models.Project;
+import ir.joboona.Models.User;
+import ir.joboona.Repositories.ProjectRepository;
+import ir.joboona.Repositories.UserRepository;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+
+@WebServlet("/project")
+public class ProjectController_ extends HttpServlet {
+
+    private final ProjectRepository projectRepository = ProjectRepository.getInstance();
+    private final UserRepository userRepo = UserRepository.getInstance();
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Optional<User> user = userRepo.getById(request.getParameter("userId"));
+
+        Set<Project> projects = projectRepository.getAll().stream()
+                .filter(project -> project.sufficientSkills(user.get().getSkills())).collect(toSet());
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World!</h3>");
+    }
+}
