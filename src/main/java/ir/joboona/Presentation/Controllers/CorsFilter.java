13a3ff569bbf477d9e930101819c42eb8b9c53bf
mehdifallahi <mohammadf00900@gmail.com>
2019-03-14 16:14:35 +0330

    add filter for cros

---
+++ src/main/java/ir/joboona/Presentation/Controllers/CorsFilter.java
@@ -0,0 +1,28 @@
+package ir.joboona.Presentation.Controllers;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/")
+public class CorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest)req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-auth-token, x-requested-with, content-type, accept, origin, referer, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "x-requested-with");
+        if(!"OPTIONS".equals(request.getMethod())) {
+            chain.doFilter(req, res);
+        }
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
